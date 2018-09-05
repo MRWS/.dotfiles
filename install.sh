bbb9950e0f73e257a402373b5d54eece721d5464
Marwan SENTENAC <m.sentenac@gmail.com>
2018-09-05 11:13:09 +0200

    add .gitignore_global

--- install.sh
+++ install.sh
@@ -15,6 +15,7 @@ if is-executable git -a -d "$DOTFILES_DIR/.git"; then git --work-tree="$DOTFILES
 # Create symlinks
 echo -e "${YELLOW}Create symlinks${NC}"
 ln -sfv "$DOTFILES_DIR/.gitconfig" ~
+ln -sfv "$DOTFILES_DIR/.gitignore_global" ~
 ln -sfv "$DOTFILES_DIR/.atom" ~
 ln -sfv "$DOTFILES_DIR/.hyper.js" ~
 ln -sfv "$DOTFILES_DIR/omf" ~/.config
